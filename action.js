499729702a615c4016a1ad2d2299cab9c5732919
Taylor Nielson <nielson.taylorm@gmail.com>
2022-08-22 14:19:31 -0600

    fix typo

--- action.js
+++ action.js
@@ -12,20 +12,27 @@ const RUN_OPTIONS_DEFAULTS = { dir: null, ignoreErrors: false, prefix: "" };
  */
 function run(cmd) {
 	core.debug(cmd);
+    const output = {
+        status: 0,
+        stdout: "",
+        stderr: "",
+    };
 	try {
 		const stdout = execSync(cmd, {
 			encoding: "utf8",
 			cwd: optionsWithDefaults.dir,
 			maxBuffer: 20 * 1024 * 1024,
 		});
-		core.debug(`Stdout: ${stdout.trim()}`);
-		return output;
+        output.stdout = stdout.trim(); 
 	} catch (error) {
+            output.status = error.status;
+            output.stderr = error.stderr;
+            output.stdout = error.stdout.trim();
 			core.debug(`Exit code: ${error.status}`);
-			core.debug(`Stdout: ${error.stdout}`);
 			core.debug(`Stderr: ${error.stderr}`);
-			return output;
 	}
+    core.debug(`Stdout: ${output.stdout}`);
+    return output;
 }
 
 module.exports = {
